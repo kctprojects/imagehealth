1b747feabee12714ec1789d2c9f1cb62d62a3090
kct <duranium>
2019-03-30 20:41:08 -0400

    framework for generating values

--- stats.py
+++ stats.py
@@ -58,36 +58,57 @@ def print_keys(data):
 def invert_scores(scores):
     return [100 - s for s in scores]
 
-def get_scores(csv, ca, ci, ce, ch):
+def get_age_scores(csv, offset):
     ages = get_data(csv, "Population,Age characteristics", "[^\(]*$")
     age_weights = [4,4,4,4,5,5,5,3.5,3.5,5,5,8,8,4.5,4.5,4.5,4.5,4.5,4.5,4.5,4.5, 4,4,4,4,5,5,5,3.5,3.5,5,5,8,8,4.5,4.5,4.5,4.5,4.5,4.5,4.5,4.5]
     age_scores = invert_scores(score_hoods(ages, age_weights))
+    return age_scores
 
+def get_income_scores(csv, offset):
     income = get_data(csv, "Income,Income of households in 2015", '.*,000.*')
     income_weights = [r for r in range(50, 29, -1)]
     income_scores = invert_scores(score_hoods(income, income_weights))
+    return income_scores
 
+def get_edu_scores(csv, offset):
     education = get_data(csv, 'Education,Highest certificate, diploma or degree')
     education_weights = [50+44, 56+51, 56+51, 58+52, 58+52, 58+52, 62+57, 62+60, 62+60, 62+60]
     ed_scores = score_hoods(education, education_weights)
+    return ed_scores
 
+def get_race_scores(csv, offset):
     race = get_data(csv, 'Visible minority,Visible minority population')
     race_weights = [78, 82, 71, 82, 78, 78, 82, 78, 82, 82, 78, 78, 77]
     race_scores = score_hoods(race, race_weights)
+    return race_scores
 
+def get_housing_scores(csv, offset):
     housing = get_data(csv, 'Housing,Household characteristics', '.*Spending.*')
     housing_weights = [1.5,1]
     housing_scores = score_hoods(housing, housing_weights)
-    
-    total = [(a + i + e + r + h) / 5 for a, i , e, r, h in zip(age_scores, income_scores, ed_scores, race_scores, housing_scores)]
-    return total
+    return housing_scores
+
+def get_codes(csv):
+    codes = get_data(csv, "Neighbourhood Information,Neighbourhood Information")
+    return codes[0][1]
+
+def get_score(ages, incomes, edus, races, housings):
+    return [(a + i + e + r + h) / 5 for a,i,e,r,h in zip(ages, incomes, edus, races, housings)]
 
 with open("2016_profiles_cleaned.csv","r") as file:
     csv = csvfy(file)
     props = {str.join(', ', row[0:2]) for row in csv}
 
-    codes = get_data(csv, "Neighbourhood Information,Neighbourhood Information")
-    print(codes[0])
-    print(min(codes[0][1]))
+    codes = get_codes(csv)
+    print(codes)
+    #print(min(codes[0][1]))
 
-    print(get_scores(csv, 0, 0, 0, 0))
+    for i in range(1):
+        ages = get_age_scores(csv, i)
+        incomes = get_income_scores(csv, i)
+        edus = get_edu_scores(csv, i)
+        races = get_race_scores(csv, i)
+        housings = get_housing_scores(csv, i)
+        scores = get_score(ages, incomes, edus, races, housings)
+        print(scores)
+        print([[c,v] for c,v in zip(codes, scores)])
